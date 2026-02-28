95bed9a772a133e221773c4b847c260c8f0af7ad
deltea <thcheetah777@gmail.com>
2026-02-28 15:54:34 -0800

    add bounce particles

--- scenes/bouncepad/bouncepad.gd
+++ scenes/bouncepad/bouncepad.gd
@@ -2,6 +2,8 @@ class_name Bouncepad extends StaticBody2D
 
 signal bounce
 
+const particles_scene = preload("res://scenes/particles/bounce_particles.tscn")
+
 @onready var sprite: Sprite2D = $Sprite
 
 @onready var scale_dynamics: DynamicsSolverVector = Dynamics.create_dynamics_vector(5.0, 0.2, 2.0)
@@ -11,3 +13,8 @@ func _process(dt: float) -> void:
 
 func _on_bounce() -> void:
 	scale_dynamics.set_value(Vector2.ONE + Vector2(0.4, -0.8))
+	var particles := particles_scene.instantiate() as CPUParticles2D
+	particles.global_position = sprite.global_position
+	particles.finished.connect(particles.queue_free)
+	particles.emitting = true
+	RoomManager.current_room.add_child(particles)
