92344ff5e6242d20de2e7cd57d89c2a45921e1e6
deltea <thcheetah777@gmail.com>
2026-02-28 00:48:34 -0800

    add scrolling background

--- scenes/camera.gd
+++ scenes/camera.gd
@@ -1,1 +1,11 @@
 class_name Camera extends Camera2D
+
+@export var follow: Node2D
+@export var follow_speed = 5.0
+
+@onready var pos_dynamics: DynamicsSolverVector = Dynamics.create_dynamics_vector(2.0, 0.8, 2.0)
+
+func _process(delta: float) -> void:
+	if follow:
+		var target_pos = follow.global_position
+		global_position = pos_dynamics.update(target_pos)
