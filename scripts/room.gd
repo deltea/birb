05c361e0555b0a9c26da7662729f35febfa77d7e
deltea <thcheetah777@gmail.com>
2026-03-01 10:31:51 -0800

    even better colors

--- scripts/room.gd
+++ scripts/room.gd
@@ -1,12 +1,20 @@
 class_name Room extends Node2D
 
 @export var palette: Texture2D
+@onready var time_label: RichTextLabel = $CanvasLayer/TimeLabel
 
 var player: Player
 var camera: Camera
+
+var is_completed = false
 
 func _enter_tree() -> void:
 	RoomManager.current_room = self
 
 func complete():
 	PaletteFilter.set_brightness(0.25)
+	is_completed = true
+
+func _process(dt: float) -> void:
+	if not is_completed:
+		time_label.text = "[wave]%.2f[/wave]" % Clock.time
