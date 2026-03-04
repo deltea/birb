60af213fac703e0822e32a73a2764437ec1508fd
deltea <thcheetah777@gmail.com>
2026-03-03 22:58:29 -0800

    add star particles!

--- scenes/collectable/collectable.gd
+++ scenes/collectable/collectable.gd
@@ -3,6 +3,8 @@ class_name Collectable extends Area2D
 @onready var sprite: Star2D = $Star2D
 
 @onready var scale_dynamics: DynamicsSolverVector = Dynamics.create_dynamics_vector(0.8, 0.2, 2.0);
+@onready var collider: CollisionShape2D = $CollisionShape2D
+@onready var particles: CPUParticles2D = $Particles
 
 var rot_speed = 1.5
 var target_scale = Vector2.ONE
@@ -11,12 +13,14 @@ func _process(dt: float) -> void:
 	sprite.rotation += dt * rot_speed
 	sprite.position.y = sin(Clock.time * 2.0) * 4.0
 	sprite.scale = scale_dynamics.update(target_scale)
+	particles.scale = sprite.scale
 	# sprite.scale = Vector2.ONE * (1.0 + sin(Clock.time * 2.0) * 0.08)
 
 	if sprite.scale <= Vector2.ZERO:
 		queue_free()
 
 func collect():
+	collider.set_deferred("disabled", true)
 	rot_speed = 5.0
 	scale_dynamics.set_value(Vector2.ONE * 1.5)
 	target_scale = Vector2.ZERO
